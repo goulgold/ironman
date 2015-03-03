ea46ba717032eb60b9c91c8add6f55c17f7b17a8
Goulgold <goulgold@gmail.com>
2015-03-03 15:08:37 -0500

    these functions are completed: 1. play youtube video Fullscreen 2. Playlist play youtube video 3. the video frame could be captured as a Bitmap Object.

--- app/src/main/java/qiming/guo/ironman/MainActivity.java
+++ app/src/main/java/qiming/guo/ironman/MainActivity.java
@@ -1,9 +1,13 @@
 package qiming.guo.ironman;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +16,31 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button full_btn = (Button) findViewById(R.id.fullscreen);
+        final Button list_btn = (Button) findViewById(R.id.search);
+
+        full_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent intent;
+                intent = new Intent(MainActivity.this,FullscreenDemoActivity.class);
+//                intent.setComponent(new ComponentName(getPackageName(), "FullscreenDemoActivity"));
+                startActivity(intent);
+
+            }
+        });
+
+        list_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent intent;
+                intent = new Intent(MainActivity.this,VideoListDemoActivity.class);
+//                intent.setComponent(new ComponentName(getPackageName(), "FullscreenDemoActivity"));
+                startActivity(intent);
+
+            }
+        });
     }
 
 
